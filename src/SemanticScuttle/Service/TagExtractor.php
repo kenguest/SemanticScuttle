8d3edffb52e486743dd2cc9c369706b4e83e1f6a
Ken Guest <ken@linux.ie>
2013-11-20 20:43:22 +0000

    Initial hack'n'slash at autotagging new bookmarks.
    Extremely basic and there should be generator/template specific drivers
    for extracting tags/categories, but it's a start.

---
+++ src/SemanticScuttle/Service/TagExtractor.php
@@ -0,0 +1,43 @@
+<?php
+class SemanticScuttle_Service_TagExtractor
+{
+    public static function getInstance($db)
+    {
+        static $instance;
+        if (!isset($instance)) {
+            $instance = new self($db);
+        }
+        return $instance;
+    }
+
+    public function extractFromUrl($url)
+    {
+        // will obviously need something better than this.
+        // It's a start though!
+        $tags = array();
+        $parsed = parse_url($url);
+        // stackexchange sites...
+        if (stripos($parsed['host'], 'stackexchange.com') !== false) {
+            $tags[] = str_replace(  '.stackexchange.com', '', $parsed['host']);
+        }
+        // check generator referenced in the source, e.g. if mediawiki then be aware of how it
+        // links to used categories, determine what they are and suggest them as tags.
+
+        // also check <meta name='keywords' content='foo,bar,qux'>
+        // <a href="foo" rel="tag">category</a>
+
+        // common/favourite keywords.
+        if (stripos($url, "ubuntu") !== false) {
+            $tags[] = "ubuntu";
+        } elseif (stripos($url, "voip") !== false) {
+            $tags[] = "voip";
+        } elseif (stripos($url, "magento") !== false) {
+            $tags[] = "magento";
+        }
+        return $tags;
+    }
+
+}
+
+
+?>
