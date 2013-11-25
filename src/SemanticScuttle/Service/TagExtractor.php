a805727035f3e24ccaa85accea5e3b5e6c054da9
Ken Guest <ken@linux.ie>
2013-11-25 08:53:38 +0000

    Coding Standards...

--- src/SemanticScuttle/Service/TagExtractor.php
+++ src/SemanticScuttle/Service/TagExtractor.php
@@ -1,4 +1,27 @@
 <?php
+/**
+ * SemanticScuttle - your social bookmark manager.
+ *
+ * PHP version 5.
+ *
+ * @category Bookmarking
+ * @package  SemanticScuttle
+ * @author   Benjamin Huynh-Kim-Bang <mensonge@users.sourceforge.net>
+ * @author   Christian Weiske <cweiske@cweiske.de>
+ * @author   Eric Dane <ericdane@users.sourceforge.net>
+ * @license  GPL http://www.gnu.org/licenses/gpl.html
+ * @link     http://sourceforge.net/projects/semanticscuttle
+ */
+
+/**
+ * Extract recommended tags for new URL not yet saved as a bookmark.
+ *
+ * @category Bookmarking
+ * @package  SemanticScuttle
+ * @author   Ken Guest <kguest@php.net>
+ * @license  GPL http://www.gnu.org/licenses/gpl.html
+ * @link     http://sourceforge.net/projects/semanticscuttle
+ */
 class SemanticScuttle_Service_TagExtractor
 {
     public static function getInstance($db)
@@ -10,6 +33,13 @@ class SemanticScuttle_Service_TagExtractor
         return $instance;
     }
 
+    /**
+     * Extract/return tags based on passed URL.
+     *
+     * @param string $url URL being saved/bookmarked.
+     *
+     * @return array
+     */
     public function extractFromUrl($url)
     {
         // will obviously need something better than this.
@@ -18,7 +48,7 @@ class SemanticScuttle_Service_TagExtractor
         $parsed = parse_url($url);
         // stackexchange sites...
         if (stripos($parsed['host'], 'stackexchange.com') !== false) {
-            $tags[] = str_replace(  '.stackexchange.com', '', $parsed['host']);
+            $tags[] = str_replace('.stackexchange.com', '', $parsed['host']);
         }
 
         $hostArray = explode('.', $parsed['host']);
@@ -27,10 +57,17 @@ class SemanticScuttle_Service_TagExtractor
             $tags[] = $hostArray[1];
         }
 
-        // check generator referenced in the source, e.g. if mediawiki then be aware of how it
-        // links to used categories, determine what they are and suggest them as tags.
+        if (strpos($parsed['path'], "/questions/tagged/") === 0) {
+            $tags[] = str_replace('+', ', ', substr($parsed['path'], 18));
+        }
+        if (strpos($parsed['path'], "/unanswered/tagged/") === 0) {
+            $tags[] = str_replace('+', ', ', substr($parsed['path'], 19));
+        }
+        // Check generator referenced in the source, e.g. if mediawiki then
+        // be aware of how it links to used categories, determine what they
+        // are and suggest them as tags.
 
-        // also check <meta name='keywords' content='foo,bar,qux'>
+        // Also check <meta name='keywords' content='foo,bar,qux'>
         // <a href="foo" rel="tag">category</a>
 
         // common/favourite keywords.
